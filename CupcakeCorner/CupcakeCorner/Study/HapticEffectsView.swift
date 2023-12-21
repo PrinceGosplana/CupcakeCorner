d1228c6d9a8813510400c97d273775a45a3aaae9
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-12-21 06:57:19 +0200

    add success haptic func

--- CupcakeCorner/CupcakeCorner/Study/HapticEffectsView.swift
+++ CupcakeCorner/CupcakeCorner/Study/HapticEffectsView.swift
@@ -29,6 +29,25 @@ struct HapticEffectsView: View {
             print("There was an error creating the engine \(error.localizedDescription)")
         }
     }
+    
+    private func complexSuccess() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        var events = [CHHapticEvent]()
+        
+        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+        events.append(event)
+        
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play patterns \(error.localizedDescription)")
+        }
+    }
 }
 
 #Preview {
